562095915226a6ae62266191a6fa08c1a6414f86
Pragati Tomar <86148240+PragatiTomar@users.noreply.github.com>
2023-07-12 12:12:25 +0530

    Update App.js

--- useState/App.js
+++ useState/App.js
@@ -1,1 +1,24 @@
-import React, { useState } from 'react' ;
+import React, {useState} from 'react';
+
+function App() {
+  const [count, setCount] = useState(4)
+  const [theme, setTheme] = useState('blue')
+  function decrementCount(){
+    setCount(prevCount => prevCount -1)
+    setTheme('blue')
+  }
+  function IncrementCount(){
+    setCount(prevCount => prevCount +1)
+    setTheme('red')
+  }
+return(
+  <>
+  <button onClick={decrementCount}>-</button>
+  <span>{count}{theme}</span>
+  <button onClick={IncrementCount}>+</button>
+  </>
+)
+     
+}
+
+export default App;
