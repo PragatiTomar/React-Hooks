63da6f75479b397ad0d1a8714d958683b5c26b3e
Pragati Tomar <86148240+PragatiTomar@users.noreply.github.com>
2023-07-12 19:30:40 +0530

    Create App.js

---
+++ useMemo/App.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect, useMemo} from 'react';
+
+function App() {
+ const [number, setNumber] = useState(0)
+ const [dark, setDark] = useState(false)
+ const doubleNumber = useMemo(() => {
+  return slowFunction(number)},[number]
+  )
+ const themeStyles = useMemo(()=> {
+  return {
+  backgroundColor: dark ? 'black' : 'white',
+  color: dark? 'white' : 'black'
+  }
+ })
+return(
+  <>
+<input type="number" value={number} onChange={e=> setNumber(parseInt(e.target.value))}/>
+<button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
+<div style={themeStyles}>{doubleNumber}</div>
+  </>
+)
+    
+        
+}
+function slowFunction(num){
+  console.log('Calling Slow Function')
+  for(let i=0; i<=1000000; i++){
+
+  }
+  return num*2;
+}
+export default App;
